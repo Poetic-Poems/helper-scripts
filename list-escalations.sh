c1e72f95da83f2b21d962b856e14e48cdc128bd5
Warwick Allen, Datum Process <warwick@datumprocess.co.nz>
2026-09-11 14:39:44 +1200

    feat: add filter to list-escalations.sh

--- list-escalations.sh
+++ list-escalations.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+# list-escalations.sh [FILTER]
+#
+# E.g.:
+#   list-escalations.sh '.repo|match("^Pullwright")'
+
 < <(
   find ~/Code/{Poetic-Poems,Pullwright} \
     -maxdepth 1 -type d -exec [ -d {}/.git ] \; -print0
@@ -8,10 +13,12 @@ for repo_dir in "${repo_dirs[@]}"; do
   owner=$(basename "$(dirname "$repo_dir")")
   repo=$owner/$(basename "$repo_dir")
   gh issue list -R"$repo" -sopen -L1000 --json id,title,labels -q 'map(
+    .repo = "'"$repo"'" |
     .labels = (.labels | map(.name)) |
-    select(.labels | any(test("enabler-escalation|pw::pager"))) |
-    .labels = (.labels | join(", ")) |
-    .repo = "'"$repo"'"
+    select(
+      (.labels | any(test("enabler-escalation|pw::pager"))) and ('"${1:-1}"')
+    ) |
+    .labels = (.labels | join(", "))
   )'
 done |
 jq -s add
