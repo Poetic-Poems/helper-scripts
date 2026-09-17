217f2037c658a166820dfe008781b4eb19b6aec1
Warwick Allen, Datum Process <warwick@datumprocess.co.nz>
2026-09-17 18:13:46 +1200

    Filter repos before calling gh in list-escalations.sh

--- list-escalations.sh
+++ list-escalations.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/bash
 
-# list-escalations.sh [FILTER]
+# list-escalations.sh [OWNER-REPO-FILTER] [JQ-FILTER]
 #
 # E.g.:
-#   list-escalations.sh '.repo|match("^Pullwright")'
+#   list-escalations.sh ^Pullwright '.number > 1000'
 
-filter="${1:-1}"
+owner_repo_filter=.
+if [ -n "$1" ]; then
+  owner_repo_filter="$1"
+  shift
+fi
+jq_filter="${1:-1}"
 < <(
   find ~/Code/{Poetic-Poems,Pullwright} \
     -maxdepth 1 -type d -exec [ -d {}/.git ] \; -print0
 ) readarray -d '' repo_dirs
 for repo_dir in "${repo_dirs[@]}"; do
   owner=$(basename "$(dirname "$repo_dir")")
-  repo=$owner/$(basename "$repo_dir")
+  owner_repo=$owner/$(basename "$repo_dir")
+  <<<"$owner_repo" grep -Eq "$owner_repo_filter" || continue
   gh-list() {
-    gh $1 list -R"$repo" -s$2 -L1000 --json id,number,title,labels -q 'map(
-      .repo = "'"$repo"'" |
+    gh $1 list -R"$owner_repo" -s$2 -L1000 --json id,number,title,labels -q 'map(
+      .repo = "'"$owner_repo"'" |
       .type = "'$1'" |
       .labels = (.labels | map(.name)) |
       select(
-        (.labels | any(test("'$3'"))) and ('"$filter"')
+        (.labels | any(test("'$3'"))) and ('"$jq_filter"')
       ) |
       .labels = (.labels | join(", "))
     )'
